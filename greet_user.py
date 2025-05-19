825ef7fd74627f8be3daf5eeb5811bde345849c0
George Lukaanya <georgelukaanya348@gmail.com>
2025-05-19 10:57:16 +0300

    First file

---
+++ greet_user.py
@@ -0,0 +1,7 @@
+#Single line comment
+'''
+Multi-line comment'''
+# This script greets the user and asks for their name and age
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+print(f"Hello, {name}! You are {age} years old.")
