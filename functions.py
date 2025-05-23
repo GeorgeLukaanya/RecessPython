37cadcbcf3d0ec210235bf860aa2cfa9e2dfbd81
George Lukaanya <georgelukaanya348@gmail.com>
2025-05-23 09:49:41 +0300

    Functions

--- functions.py
+++ functions.py
@@ -1,11 +1,6 @@
 #Function : reusable block of code
 #print() and input() are also functions
 
-# def add(a, b):
-#     return a + b
-
-# print(add(3, 4))
-
 def greet_student():
     print("Good morning, welcome to the python class!")
     
---
+++ product.py
@@ -0,0 +1,20 @@
+# #Return statement: ends function execution and sends back feedback
+
+# def product(x, y, z):
+#     return x * y * z
+
+# print(product(2, 3, 4))
+
+# def get_stats(a ,b):
+#     return a + b, a - b
+
+# sum_value, subtract_value = get_stats(20, 12)
+# print(f"Sum: {sum_value}")
+# print(f"Difference: {subtract_value}")
+
+#Exercise 2
+#Division
+def division(a, b):
+    return a/b
+
+print(division(4, 2))
